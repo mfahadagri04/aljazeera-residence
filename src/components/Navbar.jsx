626f06992aff2e94295b876ea7711ed25c817398
Muhammad Fahad <muhammad.fahad@ucalgary.ca>
2025-05-20 21:29:09 -0600

    added hamburger for smaller screens - need to adjust

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                     </a>
 
                     {/* Nav List*/}
-                    <ul className="lg:flex space-x-8 hidden text-primary">
+                    <ul className="lg:flex space-x-8 hidden text-primary cursor-pointer">
                      {navList.map(({link}) => (
                         <a key={link} className="font-medium hover:text-secondary">
                             {link}
@@ -45,9 +45,23 @@ const Navbar = () => {
                      {toggle ? (
                         <FaTimes className="w-6 h-6" />
                      ) : (
-                        <FaTimes className="w-6 h-6" />
+                        <FaBars className="w-6 h-6" />
                      )}
                     </button>
+                    
+                    {/* Hamburger Menu*/}
+                    <div
+                     className = {'${toggle ? "flex" :"hidden"} p-6 bg-primary absolute top-20 right-0 mx-4 my-2 rounded min-w-[200px]'}
+                    >
+                     {" "}
+                     <ul className="flex flex-col space-y-8 cursor-pointer">
+                     {navList.map(({link}) => (
+                        <a key={link} className="font-medium hover:text-secondary">
+                            {link}
+                        </a>
+                     ))}
+                    </ul>
+                    </div>
                 </div>
             </div>
         </nav>
