0914f19d2ea4d963c08fe72a8be199cd1fdc80b9
Muhammad Fahad <muhammad.fahad@ucalgary.ca>
2025-05-20 22:00:23 -0600

    fixed tailwin config minor error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,rs,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Matches all JS/JSX files in src/
+  ],
   theme: {
     extend: {
-      colors : {
-        primary : "#FEECE2",
-        secondary: "FFBE98",
-        tertiary: "E2BFB3",
+      colors: {
+        primary: "#FEECE2",
+        secondary: "#FFBE98",
+        tertiary: "#E2BFB3", 
       }
     },
   },
   plugins: [],
 }
-
