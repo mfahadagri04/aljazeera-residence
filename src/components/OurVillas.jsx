3e9ab2285078723682124d693eaa0db00d51a61c
Muhammad Fahad <muhammad.fahad@ucalgary.ca>
2025-05-22 23:36:28 -0600

    adjusted villa section height

--- src/components/OurVillas.jsx
+++ src/components/OurVillas.jsx
@@ -52,7 +52,7 @@ const OurVillas = () => {
   ];
 
   return (
-    <section id="OurVillas" className="py-20 px-4 md:px-16 bg-blue-50">
+    <section id="OurVillas" className="py-10 px-4 md:px-16 bg-blue-50">
       <h2 className="text-4xl font-bold text-center text-gray-800 mb-12">
         Our Villas
       </h2>
